34ce0cdff7c31620d1c90087b127b532bc09fdc5
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-03-03 15:11:40 +0300

    auth template

--- src/app/layouts/layout-auth/layout-auth.component.ts
+++ src/app/layouts/layout-auth/layout-auth.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-layout-auth',
-  imports: [
-    RouterOutlet
-  ],
+    imports: [
+        RouterOutlet,
+        NgOptimizedImage
+    ],
   templateUrl: './layout-auth.component.html',
   styles: ``
 })
--- src/app/pages/authentification/login/login.component.ts
+++ src/app/pages/authentification/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styles: ``
 })
